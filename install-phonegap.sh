2a9bcf7be0bdb446cdde9474e526fc4de0f9814e
serkansevilgen <ssevilgen@gmail.com>
2014-08-30 13:25:23 +0300

    Create install-phonegap.sh

---
+++ install-phonegap.sh
@@ -0,0 +1,10 @@
+# NODE.JS
+echo "~~~ INSTALLING NODEJS express forever jshint ~~~"
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update > /dev/null
+sudo apt-get install nodejs
+
+npm install -g phonegap
+npm install -g cordova
+
+sudo apt-get install ant
